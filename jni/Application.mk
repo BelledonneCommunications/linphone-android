62ad089279b83ace70cc2f1d2e10e59796500fc0
Simon Morlat <simon.morlat@linphone.org>
2010-10-24 22:28:22 +0200

    android display builds correctly

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,12 +1,12 @@
 APP_PROJECT_PATH := $(call my-dir)/../
-APP_MODULES      :=libspeex libgsm libortp libosip2 libeXosip2 libmediastreamer2  liblinphone
+APP_MODULES      :=libspeex libgsm libortp libosip2 libeXosip2 libmediastreamer2  liblinphone msandroiddisplay
 
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-APP_MODULES      +=libmsilbc libavutil libavcore libavcodec libswscale
+APP_MODULES      +=libmsilbc libavutil libavcore libavcodec libswscale 
 endif
 APP_BUILD_SCRIPT:=$(call my-dir)/Android.mk
-APP_PLATFORM := android-3
+APP_PLATFORM := android-8
 APP_ABI := armeabi armeabi-v7a
 #APP_OPTIM := debug
 
