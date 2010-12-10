3efbb0b44d5ca7a1640c52c7207c84c944cb4c65
Jehan Monnier <jehan.monnier@linphone.org>
2010-12-10 16:41:14 +0100

    add optimized resampler

--- submodules/externals/build/speex/speex/speex_config_types.h
+++ submodules/externals/build/speex/speex/speex_config_types.h
@@ -7,5 +7,7 @@ typedef unsigned short spx_uint16_t;
 typedef int spx_int32_t;
 typedef unsigned int spx_uint32_t;
 
+extern float ff_scalarproduct_float_neon  (const float *v1, const float *v2, int len);
+extern float interpolate_product_single(const float *a, const float *b, unsigned int len, const spx_uint32_t oversample, float *frac);
 #endif
 
