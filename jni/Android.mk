1b0cb40009d23d928b32497892cae0aa6cd45320
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@linphone.org>
2011-09-13 15:44:48 +0200

    android: updated build scripts

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,13 @@
-root-dir:=$(APP_PROJECT_PATH)
+# script expect linphone-root-dir variable to be set by parent !
+
 #default values
 BUILD_AMR=light
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 BUILD_X264=1
 LINPHONE_VIDEO=1
+else
+LINPHONE_VIDEO=0
+BUILD_X264=0
 endif
 
 
@@ -11,93 +15,90 @@ endif
 ifeq ($(BUILD_GPLV3_ZRTP), 1)
 BUILD_SRTP=1
 ZRTP_C_INCLUDE= \
-	$(root-dir)/submodules/externals/libzrtpcpp/src
+	$(linphone-root-dir)/submodules/externals/libzrtpcpp/src
 endif
 
 ifeq ($(BUILD_SRTP), 1)
 SRTP_C_INCLUDE= \
-	$(root-dir)/submodules/externals/srtp/include \
-	$(root-dir)/submodules/externals/srtp/crypto/include
+	$(linphone-root-dir)/submodules/externals/srtp/include \
+	$(linphone-root-dir)/submodules/externals/srtp/crypto/include
 endif
 #endif
 
 
 # Speex
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/speex.mk),)
-include $(root-dir)/submodules/externals/build/speex/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/speex.mk),)
+include $(linphone-root-dir)/submodules/externals/build/speex/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/speex.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/speex.mk
 endif
 
 # Gsm
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/gsm.mk),)
-include $(root-dir)/submodules/externals/build/gsm/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/gsm.mk),)
+include $(linphone-root-dir)/submodules/externals/build/gsm/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/gsm.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/gsm.mk
 endif
 
-include $(root-dir)/submodules/externals/build/exosip/Android.mk
+include $(linphone-root-dir)/submodules/externals/build/exosip/Android.mk
 
-include $(root-dir)/submodules/externals/build/osip/Android.mk
+include $(linphone-root-dir)/submodules/externals/build/osip/Android.mk
 
 # Openssl
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/ssl.mk),)
-include $(root-dir)/submodules/externals/openssl/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/ssl.mk),)
+include $(linphone-root-dir)/submodules/externals/openssl/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/ssl.mk
-include $(root-dir)/submodules/externals/prebuilts/crypto.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/ssl.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/crypto.mk
 endif
 
 
-include $(root-dir)/submodules/linphone/oRTP/build/android/Android.mk
+include $(linphone-root-dir)/submodules/linphone/oRTP/build/android/Android.mk
 
-include $(root-dir)/submodules/linphone/mediastreamer2/build/android/Android.mk
-include $(root-dir)/submodules/linphone/mediastreamer2/tests/Android.mk
+include $(linphone-root-dir)/submodules/linphone/mediastreamer2/build/android/Android.mk
+include $(linphone-root-dir)/submodules/linphone/mediastreamer2/tests/Android.mk
 
 
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-include $(root-dir)/submodules/msilbc/Android.mk
+include $(linphone-root-dir)/submodules/msilbc/Android.mk
 
 ifeq ($(BUILD_X264), 1)
-include $(root-dir)/submodules/msx264/Android.mk
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/x264.mk),)
-include $(root-dir)/submodules/externals/build/x264/Android.mk
+include $(linphone-root-dir)/submodules/msx264/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/x264.mk),)
+include $(linphone-root-dir)/submodules/externals/build/x264/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/x264.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/x264.mk
 endif
 endif
 
 
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/ffmpeg.mk),)
-include $(root-dir)/submodules/externals/build/ffmpeg/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/ffmpeg.mk),)
+include $(linphone-root-dir)/submodules/externals/build/ffmpeg/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/ffmpeg.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/ffmpeg.mk
 endif
 endif #armeabi-v7a
 
-include $(root-dir)/submodules/externals/build/libvpx/Android.mk
+include $(linphone-root-dir)/submodules/externals/build/libvpx/Android.mk
 
 
 ifeq ($(BUILD_GPLV3_ZRTP), 1)
-ifeq ($(wildcard $(root-dir)/submodules/externals/prebuilts/zrtpcpp.mk),)
-include $(root-dir)/submodules/externals/build/libzrtpcpp/Android.mk
+ifeq ($(wildcard $(linphone-root-dir)/submodules/externals/prebuilts/zrtpcpp.mk),)
+include $(linphone-root-dir)/submodules/externals/build/libzrtpcpp/Android.mk
 else
-include $(root-dir)/submodules/externals/prebuilts/zrtpcpp.mk
+include $(linphone-root-dir)/submodules/externals/prebuilts/zrtpcpp.mk
 endif
 endif
 
 ifeq ($(BUILD_SRTP), 1)
-include $(root-dir)/submodules/externals/build/srtp/Android.mk
+include $(linphone-root-dir)/submodules/externals/build/srtp/Android.mk
 endif
 
 
-include $(root-dir)/submodules/linphone/build/android/Android.mk
+include $(linphone-root-dir)/submodules/linphone/build/android/Android.mk
 
 ifneq ($(BUILD_AMR), 0)
-include $(root-dir)/submodules/externals/build/opencore-amr/Android.mk
-include $(root-dir)/submodules/msamr/Android.mk
+include $(linphone-root-dir)/submodules/externals/build/opencore-amr/Android.mk
+include $(linphone-root-dir)/submodules/msamr/Android.mk
 endif
-
-
-
--- jni/Application.mk
+++ jni/Application.mk
@@ -3,7 +3,7 @@ APP_MODULES      :=libspeex libgsm libortp libosip2 libeXosip2 libmediastreamer2
 APP_STL := stlport_static
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-APP_MODULES      +=libmsilbc 
+APP_MODULES      +=libmsilbc
 endif
 
 ifeq ($(LINPHONE_VIDEO),1)
@@ -36,8 +36,9 @@ APP_MODULES      += libsrtp
 endif
 endif
 
+linphone-root-dir:=$(APP_PROJECT_PATH)
 
 APP_BUILD_SCRIPT:=$(call my-dir)/Android.mk
 APP_PLATFORM := android-8
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi-v7a armeabi
 APP_CFLAGS:=-DDISABLE_NEON
