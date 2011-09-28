a71187d7cd2af86425b0f867449b19fbbc6d6fff
Simon Morlat <simon.morlat@linphone.org>
2011-09-28 11:31:57 +0200

    fix prepare_sources.sh to report errors better

--- prepare_sources.sh
+++ prepare_sources.sh
@@ -2,8 +2,8 @@
 
 topdir=`pwd`
 
-cd submodules/libilbc-rfc3951 && ./autogen.sh && ./configure && make || ( echo "iLBC prepare stage failed" ; exit -1 )
+cd submodules/libilbc-rfc3951 && ./autogen.sh && ./configure && make || ( echo "iLBC prepare stage failed" ; exit 1 )
 
-cd $topdir/submodules/externals/build/libvpx && ./asm_conversion.sh && cp *.asm *.h ../../libvpx/ || ( echo "VP8 prepare stage failed." ; exit -1 )
+cd $topdir/submodules/externals/build/libvpx && ./asm_conversion.sh && cp *.asm *.h ../../libvpx/ || ( echo "VP8 prepare stage failed." ; exit 1 )
 
 
