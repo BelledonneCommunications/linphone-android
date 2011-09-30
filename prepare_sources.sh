4b5dba856a1e76184ab08567ffd41ac971b8c2de
Simon Morlat <simon.morlat@linphone.org>
2011-09-30 21:31:07 +0200

    integrate ffmpeg patch to

--- prepare_sources.sh
+++ prepare_sources.sh
@@ -2,6 +2,14 @@
 
 topdir=`pwd`
 
+cd submodules/externals/ffmpeg
+if test -z "`git status | grep neon`" ; then
+	echo "Applying patch to ffmpeg"
+	cd $topdir
+	patch -p0 < ${topdir}/patches/ffmpeg_scalar_product_remove_alignment_hints.patch
+fi
+cd $topdir
+
 cd submodules/libilbc-rfc3951 && ./autogen.sh && ./configure && make || ( echo "iLBC prepare stage failed" ; exit 1 )
 
 cd $topdir/submodules/externals/build/libvpx && ./asm_conversion.sh && cp *.asm *.h ../../libvpx/ || ( echo "VP8 prepare stage failed." ; exit 1 )
