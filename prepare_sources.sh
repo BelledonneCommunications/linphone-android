44084a39ce8b8f26d246ad3308633f5e0937150a
Jehan Monnier <jehan.monnier@linphone.org>
2011-10-31 16:26:36 +0100

    remove dependency on ms2 for prepare source

--- prepare_sources.sh
+++ prepare_sources.sh
@@ -13,7 +13,7 @@ cd $topdir/submodules/libilbc-rfc3951 && ./autogen.sh && ./configure && make || 
 
 cd $topdir/submodules/externals/build/libvpx && ./asm_conversion.sh && cp *.asm *.h ../../libvpx/ || ( echo "VP8 prepare stage failed." ; exit 1 )
 
-cd $topdir/submodules/mssilk && ./autogen.sh && ./configure && cd sdk && make extract-sources || ( echo "SILK audio plugin prepare state failed." ; exit 1 )
+cd $topdir/submodules/mssilk && ./autogen.sh && ./configure MEDIASTREAMER_CFLAGS=" " MEDIASTREAMER_LIBS=" " && cd sdk && make extract-sources || ( echo "SILK audio plugin prepare state failed." ; exit 1 )
 
 
 # As a memo, the config.h for zrtpcpp is generated using the command
