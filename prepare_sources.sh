29e52cdab4dbe1d553c9bd9a076786732612f9ee
Simon Morlat <simon.morlat@linphone.org>
2012-03-31 16:56:27 +0200

    repair prepare_sources.sh (should not ask for NDK path !)

--- prepare_sources.sh
+++ prepare_sources.sh
@@ -2,11 +2,17 @@
 
 topdir=`pwd`
 
-if [ $# -ne 1 ] 
-then
-	"Error : path to the ndk as parameter."
-	exit 65
+if test -z "$1" ; then
+	ndk_build_path=`which ndk-build`
+	NDK_PATH=`dirname $ndk_build_path`
+	if test -z "NDK_PATH" ; then
+		echo "Path to Android NDK not set, please specify it as first argument of $0"
+	fi
+else
+	NDK_PATH=$1
 fi
+
+echo "using $NDK_PATH as android NDK"
 
 cd submodules/externals/ffmpeg
 if test -z "`git status | grep neon`" ; then
@@ -17,9 +23,10 @@ fi
 
 cd $topdir/submodules/libilbc-rfc3951 && ./autogen.sh && ./configure && make || ( echo "iLBC prepare stage failed" ; exit 1 )
 
-cd $topdir/submodules/externals/libvpx && ./configure --target=armv7-android-gcc --sdk-path=$1 --enable-error-concealment && make asm_com_offsets.asm || ( echo "VP8 prepare stage failed." ; exit 1 )
+cd $topdir/submodules/externals/libvpx && ./configure --target=armv7-android-gcc --sdk-path=$NDK_PATH --enable-error-concealment && make asm_com_offsets.asm || ( echo "VP8 prepare stage failed." ; exit 1 )
 
 cd $topdir/submodules/mssilk && ./autogen.sh && ./configure --host=arm-linux MEDIASTREAMER_CFLAGS=" " MEDIASTREAMER_LIBS=" " && cd sdk && make extract-sources || ( echo "SILK audio plugin prepare state failed." ; exit 1 )
 
 # As a memo, the config.h for zrtpcpp is generated using the command
 # cmake  -Denable-ccrtp=false submodules/externals/libzrtpcpp
+
