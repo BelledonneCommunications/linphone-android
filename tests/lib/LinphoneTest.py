f1d2439254634e50251aab50282cfa954ddc5491
Sylvain Berfini <Sylvain.Berfini@Belledonne-Communications.com>
2012-09-04 14:43:08 +0200

    Test lib + samples added

---
+++ tests/lib/LinphoneTest.py
@@ -0,0 +1,55 @@
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+from com.android.monkeyrunner.easy import EasyMonkeyDevice
+from com.android.monkeyrunner.easy import By
+
+class colors:
+    OK = '\033[92m'
+    KO = '\033[91m'
+    END = '\033[0m'
+    
+class LinphoneTest():
+	def __init__(self, test_name):
+		self.test_name = test_name
+		
+		# Connects to the current device
+		self.device = MonkeyRunner.waitForConnection()
+		self.easyDevice = EasyMonkeyDevice(self.device)
+		
+	def run(self):
+		self.precond()
+		
+		try:			
+			result = self.test()
+			if result :
+				self.print_result_ok()
+			else :
+				self.print_result_ko()
+				
+		except Exception:
+			self.print_result_ko()
+			
+		finally:
+			self.postcond()
+			
+	def find(self, id):
+		view = By.id('id/' + id)
+		if not view:
+			raise Exception("View with id/" + id + " not found")
+		return view
+		
+	def print_result_ok(self):
+		print self.test_name + colors.OK + ' OK' + colors.END
+		
+	def print_result_ko(self):
+		print self.test_name + colors.KO + ' KO' + colors.END
+		
+	# Override following methods
+	def precond(self):
+		pass
+		
+	def test(self):
+		pass
+		
+	def postcond(self):
+		pass
+		
