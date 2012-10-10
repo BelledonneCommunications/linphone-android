3c36b055f993020789cf4cb44ab4489191535111
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2012-10-10 16:03:28 +0200

    Proper fix to avoid storing contact in duplicate object

--- src/org/linphone/LinphoneActivity.java
+++ src/org/linphone/LinphoneActivity.java
@@ -1063,35 +1063,24 @@ public class LinphoneActivity extends FragmentActivity implements OnClickListene
 					}
 					sipContactList.add(contact);
 				}
+				for (int i = 0; i < contactCursor.getCount(); i++) {
+					Contact contact = Compatibility.getContact(getContentResolver(), contactCursor, i);
+					Log.e("Contact ID = " + contact.getID());
+					for (Contact c : sipContactList) {
+						if (c.getID().equals(contact.getID())) {
+							Log.e("Match found : Temp ID = " + c.getID());
+							contact = c;
+							break;
+						}
+					}
+					contactList.add(contact);
+				}
 			}
 		});
 		sipContactsHandler.start();
 			
 		contactList = new ArrayList<Contact>();
 		sipContactList = new ArrayList<Contact>();
-		
-		Thread contactsHandler = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				for (int i = 0; i < contactCursor.getCount(); i++) {
-					Contact contact = Compatibility.getContact(getContentResolver(), contactCursor, i);
-//						contact.refresh(getContentResolver());
-//						
-//						for (String aon : contact.getNumerosOrAddresses()) {
-//							if (LinphoneUtils.isSipAddress(aon)) {
-//								if (!isContactPresenceDisabled) {
-//									searchFriendAndAddToContact(contact);
-//								}
-//								if (!sipContactList.contains(contact)) {
-//									sipContactList.add(contact);
-//								}
-//							}
-//						}
-					contactList.add(contact);
-				}
-			}
-		});
-		contactsHandler.start();
 	}
 	
 	private void initInCallMenuLayout(boolean callTransfer) {
