5860fd54cc3ca448bd7450752db0fc7490f62b66
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2012-12-10 17:00:35 +0100

    Do not build for x86 by default.

--- jni/Application.mk
+++ jni/Application.mk
@@ -79,6 +79,8 @@ linphone-root-dir:=$(APP_PROJECT_PATH)
 
 APP_BUILD_SCRIPT:=$(call my-dir)/Android.mk
 APP_PLATFORM := android-8
-#APP_ABI := armeabi
-APP_ABI := armeabi-v7a armeabi x86
+APP_ABI := armeabi-v7a armeabi
+ifeq ($(BUILD_FOR_X86), 1)
+APP_ABI += x86
+endif
 APP_CFLAGS:=-DDISABLE_NEON
