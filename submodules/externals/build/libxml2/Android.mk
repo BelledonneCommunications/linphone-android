2f6a101e54f6df39857196012d67d60b40ed0ce1
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-02-05 10:04:00 +0100

    Add xml2lpc/lpc2xml support and remote provisioning

---
+++ submodules/externals/build/libxml2/Android.mk
@@ -0,0 +1,68 @@
+LOCAL_PATH:= $(call my-dir)/../../libxml2
+
+# We need to build this for both the device (as a shared library)
+# and the host (as a static library for tools to use).
+
+common_SRC_FILES := \
+	SAX.c \
+	entities.c \
+	encoding.c \
+	error.c \
+	parserInternals.c \
+	parser.c \
+	tree.c \
+	hash.c \
+	list.c \
+	xmlIO.c \
+	xmlmemory.c \
+	uri.c \
+	valid.c \
+	xlink.c \
+	HTMLparser.c \
+	HTMLtree.c \
+	debugXML.c \
+	xpath.c \
+	xpointer.c \
+	xinclude.c \
+	nanohttp.c \
+	nanoftp.c \
+	DOCBparser.c \
+	catalog.c \
+	globals.c \
+	threads.c \
+	c14n.c \
+	xmlstring.c \
+	xmlregexp.c \
+	xmlschemas.c \
+	xmlschemastypes.c \
+	xmlunicode.c \
+	xmlreader.c \
+	relaxng.c \
+	dict.c \
+	SAX2.c \
+	legacy.c \
+	chvalid.c \
+	pattern.c \
+	xmlsave.c \
+	xmlmodule.c \
+	xmlwriter.c \
+	schematron.c
+
+common_C_INCLUDES += \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/../build/libxml2  \
+
+# For the device
+# =====================================================
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(common_SRC_FILES)
+LOCAL_C_INCLUDES += $(common_C_INCLUDES)
+LOCAL_SHARED_LIBRARIES += $(common_SHARED_LIBRARIES)
+#LOCAL_CFLAGS += -fvisibility=hidden
+
+LOCAL_MODULE:= libxml2
+
+include $(BUILD_SHARED_LIBRARY)
+
