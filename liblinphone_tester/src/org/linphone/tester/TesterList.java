feef30b5958d7945e79074cd0395d06ab196a727
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-03-08 16:09:35 +0100

    Add suites and tests in liblinphone_tester

---
+++ liblinphone_tester/src/org/linphone/tester/TesterList.java
@@ -0,0 +1,16 @@
+package org.linphone.tester;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TesterList extends Tester {
+	private List<String> list = new LinkedList<String>();
+	public void printLog(final int level, final String message) {
+		super.printLog(level, message);
+		list.add(message);
+	}
+	
+	public List<String> getList() {
+		return list;
+	}
+}
