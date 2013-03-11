47d0c15be2f66f2262d561070094dccd831461b8
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-03-11 15:47:57 +0100

    Improve liblinphone_tester

--- liblinphone_tester/src/org/linphone/tester/MainActivity.java
+++ liblinphone_tester/src/org/linphone/tester/MainActivity.java
@@ -13,7 +13,6 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity extends Activity {
 
