583c1ea67246581d374999aa283722d9c7bd9166
Guillaume Beraudo <guillaume.beraudo@belledonne-communications.com>
2013-06-05 11:19:56 +0200

    Setup activity: customize for direct config + forced proxy
    Also:
    - replace deprecated use of onKeyDown
    - custom for moveToBack
    
    Limitations:
    - still no checking of proper registration before exiting wizard.

--- src/org/linphone/setup/SetupFragment.java
+++ src/org/linphone/setup/SetupFragment.java
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /**
  * @author Sylvain Berfini
  */
-public enum SetupFragments {
+public enum SetupFragment {
 	WELCOME,
 	MENU,
 	WIZARD,
