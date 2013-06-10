fcb4ebc5ddba26d3fa95e18fc2ee9b80faecfd2a
Sylvain Berfini <sylvain.berfini@linphone.org>
2013-06-10 09:47:35 +0200

    Few fixes

--- src/org/linphone/setup/SetupFragmentsEnum.java
+++ src/org/linphone/setup/SetupFragmentsEnum.java
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 /**
  * @author Sylvain Berfini
  */
-public enum SetupFragment {
+public enum SetupFragmentsEnum {
 	WELCOME,
 	MENU,
 	WIZARD,
