897aef72ec9a4a6e80b87c027850170511a8993e
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-09-27 11:34:06 +0200

    Fix handling of video build flags.

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,13 +4,18 @@ include $(linphone-root-dir)/submodules/linphone/mediastreamer2/src/android/libn
 
 
 #enable video on armv7 and x86 targets only
+#since we want to modify BUILD_VIDEO and BUILD_X264 depending on platform, we need to make a copy because the
+#variables given on command line take precedence over the ones defined internally.
 ifeq ($(TARGET_ARCH_ABI), armeabi)
 _BUILD_X264=0
 _BUILD_VIDEO=0
+else
+_BUILD_X264=$(BUILD_X264)
+_BUILD_VIDEO=$(BUILD_VIDEO)
 endif
 ifeq ($(_BUILD_VIDEO),0)
 ifeq (,$(DUMP_VAR))
-$(info Video is disabled for targets other than armeabi-v7a and x86)
+$(info $(TARGET_ARCH_ABI): Video is disabled for targets other than armeabi-v7a and x86)
 endif
 endif
 
@@ -75,7 +80,7 @@ endif
 
 ifeq ($(BUILD_SILK), 1)
 ifeq (,$(DUMP_VAR))
-$(info Build proprietary SILK plugin for mediastreamer2)
+$(info $(TARGET_ARCH_ABI): Build proprietary SILK plugin for mediastreamer2)
 endif
 include $(linphone-root-dir)/submodules/mssilk/Android.mk
 endif
@@ -86,7 +91,7 @@ ifeq ($(_BUILD_VIDEO),1)
 
 ifeq ($(_BUILD_X264),1)
 ifeq (,$(DUMP_VAR))
-$(info Build X264 plugin for mediastreamer2)
+$(info $(TARGET_ARCH_ABI): Build X264 plugin for mediastreamer2)
 endif
 include $(linphone-root-dir)/submodules/msx264/Android.mk
 include $(linphone-root-dir)/submodules/externals/build/x264/Android.mk
@@ -100,7 +105,7 @@ endif #_BUILD_VIDEO
 
 ifeq ($(BUILD_GPLV3_ZRTP), 1)
 ifeq (,$(DUMP_VAR))
-$(info Build ZRTP support - makes application GPLv3)
+$(info $(TARGET_ARCH_ABI): Build ZRTP support - makes application GPLv3)
 endif
 include $(linphone-root-dir)/submodules/externals/build/libzrtpcpp/Android.mk
 endif
