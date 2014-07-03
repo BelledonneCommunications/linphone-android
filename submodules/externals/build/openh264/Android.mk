52a3f4a3468afc92d2460553837f3ee13bb96123
Sylvain Berfini <sylvain.berfini@linphone.org>
2014-07-03 10:41:01 +0200

    Fix Android build

--- submodules/externals/build/openh264/Android.mk
+++ submodules/externals/build/openh264/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libwels
-LOCAL_SRC_FILES := $(TARGET_ARCH)/libwels.a
+LOCAL_MODULE := libopenh264
+LOCAL_SRC_FILES := $(TARGET_ARCH)/libopenh264.a
 
 include $(PREBUILT_STATIC_LIBRARY)
