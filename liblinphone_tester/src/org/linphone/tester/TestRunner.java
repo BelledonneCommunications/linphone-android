5e105aec46c0abb693114795ac56ba39276dee59
Jehan Monnier <jehan.monnier@linphone.org>
2015-02-05 16:04:06 +0100

    add jni wrapper junit test case

--- liblinphone_tester/src/org/linphone/tester/TestRunner.java
+++ liblinphone_tester/src/org/linphone/tester/TestRunner.java
@@ -30,6 +30,7 @@ public class TestRunner extends JUnitReportTestRunner {
 	@Override
 	public TestSuite getAllTests () {
 		TestSuite suite = new TestSuite("Tests");
+		suite.addTest(new WrapperTester());
 		addSuites(suite, mSuite, mTest);
 		return suite;
 	}
