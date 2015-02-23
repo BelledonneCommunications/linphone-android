fab162712124e731547019b4bd3e481c16badf5c
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-02-23 09:35:47 +0100

    Android.mk: Turn off warnings for libvpx

--- submodules/externals/build/libvpx/Android.mk
+++ submodules/externals/build/libvpx/Android.mk
@@ -4,4 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libvpx
 LOCAL_SRC_FILES := $(TARGET_ARCH)/libvpx.a
 
+#turn off warnings since we cannot fix them
+LOCAL_CFLAGS += -w
+
 include $(PREBUILT_STATIC_LIBRARY)
