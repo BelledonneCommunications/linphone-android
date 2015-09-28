75673e549957559808208d38a33d354e8c15149a
Gautier Pelloux-Prayer <gautier.pelloux-prayer@belledonne-communications.com>
2015-09-28 10:42:58 +0200

    submodules: update belle-sip and linphone and fix liblinphone tester

--- liblinphone_tester/src/org/linphone/tester/TestUnit.java
+++ liblinphone_tester/src/org/linphone/tester/TestUnit.java
@@ -16,28 +16,28 @@ public class TestUnit extends AndroidTestCase {
 	private String mSuite;
 	private String mTest;
 	static Boolean isAssetCopied=false;
-	
+
 	public TestUnit(String suite, String test) {
 		mSuite = suite;
 		mTest = test;
 		setName(suite + "/" + test);
 	}
-	
+
 	public TestUnit(String name) {
-		String[] tab = name.split("/"); 
+		String[] tab = name.split("/");
 		mSuite = tab[0];
 		mTest = tab[1];
 		setName(name);
 	}
-	
+
 	static public void copyAssetsFromPackage(Context ctx) throws IOException {
 		copyAssetsFromPackage(ctx,"config_files");
 	}
-	
-	
+
+
 	public static void copyAssetsFromPackage(Context ctx,String fromPath) throws IOException {
 		new File(ctx.getFilesDir().getPath()+"/"+fromPath).mkdir();
-		
+
 		for (String f :ctx.getAssets().list(fromPath)) {
 			String current_name=fromPath+"/"+f;
 			InputStream lInputStream;
@@ -50,8 +50,8 @@ public class TestUnit extends AndroidTestCase {
 				continue;
 			}
 			FileOutputStream lOutputStream =  new FileOutputStream(new File(ctx.getFilesDir().getPath()+"/"+current_name));//ctx.openFileOutput (fromPath+"/"+f, 0);
-			
-			
+
+
 			int readByte;
 			byte[] buff = new byte[8048];
 			while (( readByte = lInputStream.read(buff)) != -1) {
@@ -75,12 +75,14 @@ public class TestUnit extends AndroidTestCase {
 	protected void tearDown() throws Exception {
 		super.tearDown();
 	}
-	
+
 	@Override
 	protected void runTest() {
-		String path = getContext().getFilesDir().getPath()+"/config_files";
+		String res_path = getContext().getFilesDir().getPath()+"/config_files";
+		String write_path = getContext().getCacheDir().getPath();
 		Tester tester = new Tester();
-		List<String> list = new LinkedList<String>(Arrays.asList(new String[]{"tester", "--verbose", "--resource-dir", path, "--suite", mSuite, "--test", mTest}));
+
+		List<String> list = new LinkedList<String>(Arrays.asList(new String[]{"tester", "--verbose", "--resource-dir", res_path, "--writable-dir", write_path, "--suite", mSuite, "--test", mTest}));
 		String[] array = list.toArray(new String[list.size()]);
 		Assert.assertTrue(tester.run(array) == 0);
 	}
