33363b67b3a352b9f46009a8bd7314572403f875
Margaux Clerc <margaux.clerc@belledonne-communications.com>
2015-11-17 17:22:51 +0100

    Add mask for incoming cat bubble

--- src/org/linphone/AboutFragment.java
+++ src/org/linphone/AboutFragment.java
@@ -29,7 +29,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 /**
