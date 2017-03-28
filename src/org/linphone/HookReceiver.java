48f0eea1e1e3b88557e4739e34400cece8a07331
Brieuc Viel <brieuc.viel@belledonne-communications.com>
2017-03-28 16:52:14 +0200

    [Handset] hookReceiver java class added

---
+++ src/org/linphone/HookReceiver.java
@@ -0,0 +1,31 @@
+package org.linphone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import org.linphone.mediastream.Log;
+
+public class HookReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(isOrderedBroadcast())
+            abortBroadcast();
+        Bundle extras = intent.getExtras();
+        boolean b = extras.getBoolean("hookoff");
+        if(b){
+            //handset on
+            Log.i(" ======>>>>>> HookReceiver - handset ON");
+            LinphoneManager.getLc().enableSpeaker(false);
+            LinphoneManager.getInstance().setHandsetMode(true);
+
+
+        }else{
+            //handset off
+            Log.i(" ======>>>>>> HookReceiver - handset OFF");
+            LinphoneManager.getLc().enableSpeaker(true);
+            LinphoneManager.getInstance().setHandsetMode(false);
+        }
+    }
+}
