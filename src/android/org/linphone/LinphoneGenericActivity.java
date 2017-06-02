2d39a0c22cd087a094bdf994ffdf327ea6fa5189
Erwan Croze <erwan.croze@belledonne-communications.com>
2017-06-02 14:57:50 +0200

    Try to fix LinphoneService not ready crash

--- src/android/org/linphone/LinphoneGenericActivity.java
+++ src/android/org/linphone/LinphoneGenericActivity.java
@@ -12,6 +12,11 @@ public class LinphoneGenericActivity extends Activity {
         /*After a crash, Android restart the last Activity so we need to check
         * if all dependencies are load
         */
+        if (!LinphoneService.isReady()) {
+            finish();
+            startService(getIntent().setClass(this, LinphoneService.class));
+            return;
+        }
         if (!LinphoneManager.isInstanciated()) {
             finish();
             startActivity(getIntent().setClass(this, LinphoneLauncherActivity.class));
