bdbf5f6bd67528cd8665410c0cd08c2b20488ed2
Aniruddh Bhilvare <anu.bhilvare@gmail.com>
2017-10-12 17:32:56 +0530

    Solved DisplayedName to be overridden by setText.
    If we call setText after setDisplayedName of AddressText, it will clear value of displayedName as we are callling clearDisplayedName in onTextChanged method of AddressText.

--- src/android/org/linphone/LinphoneActivity.java
+++ src/android/org/linphone/LinphoneActivity.java
@@ -1000,8 +1000,8 @@ public class LinphoneActivity extends LinphoneGenericActivity implements OnClick
 //		changeCurrentFragment(FragmentsAvailable.DIALER, extras);
 
 		AddressType address = new AddressText(this, null);
-		address.setDisplayedName(name);
 		address.setText(number);
+		address.setDisplayedName(name);		
 		LinphoneManager.getInstance().newOutgoingCall(address);
 	}
 
