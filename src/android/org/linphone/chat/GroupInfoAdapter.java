a9aa99e5b849ff854673d7b546adc610228c7a5a
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2017-11-13 11:13:54 +0100

    Always display which participant is admin in group chat info

--- src/android/org/linphone/chat/GroupInfoAdapter.java
+++ src/android/org/linphone/chat/GroupInfoAdapter.java
@@ -88,9 +88,9 @@ public class GroupInfoAdapter extends BaseAdapter {
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mDeleteListener != null) {
-                    mDeleteListener.onClick(view);
-                }
+            if (mDeleteListener != null) {
+                mDeleteListener.onClick(view);
+            }
             }
         });
         delete.setTag(ca);
@@ -101,28 +101,26 @@ public class GroupInfoAdapter extends BaseAdapter {
         isAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isNotAdmin.setVisibility(View.VISIBLE);
-                isAdmin.setVisibility(View.GONE);
-                ca.setAdmin(false);
+            isNotAdmin.setVisibility(View.VISIBLE);
+            isAdmin.setVisibility(View.GONE);
+            ca.setAdmin(false);
             }
         });
 
         isNotAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                isNotAdmin.setVisibility(View.GONE);
-                isAdmin.setVisibility(View.VISIBLE);
-                ca.setAdmin(true);
+            isNotAdmin.setVisibility(View.GONE);
+            isAdmin.setVisibility(View.VISIBLE);
+            ca.setAdmin(true);
             }
         });
 
 	    delete.setVisibility(View.VISIBLE);
-	    adminLayout.setVisibility(View.VISIBLE);
         if (mHideAdminFeatures) {
-            delete.setVisibility(View.GONE);
-            adminLayout.setVisibility(View.GONE);
-        } else if (mIsCreation) {
-	        adminLayout.setVisibility(View.GONE);
+            delete.setVisibility(View.INVISIBLE);
+            isAdmin.setOnClickListener(null); // Do not allow not admin to remove it's rights but display admins
+            isNotAdmin.setVisibility(View.GONE); // Hide not admin button for not admin participants
         }
 
         return view;
