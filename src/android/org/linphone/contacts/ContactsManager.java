2948b31ff383760fb777181eaa55ec747c51349e
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2017-12-01 11:04:22 +0100

    Fixed contact lookup in message notification

--- src/android/org/linphone/contacts/ContactsManager.java
+++ src/android/org/linphone/contacts/ContactsManager.java
@@ -212,7 +212,6 @@ public class ContactsManager extends ContentObserver {
 	}
 
 	public synchronized LinphoneContact findContactFromAddress(Address address) {
-		String sipUri = address.asStringUriOnly();
 		Core lc = LinphoneManager.getLcIfManagerNotDestroyedOrNull();
 		Friend lf = lc.findFriend(address);
 		if (lf != null) {
