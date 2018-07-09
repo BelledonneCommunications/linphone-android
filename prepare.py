4821fc1aea4e07151fe25b6b52387bb283517435
Erwan Croze <erwan.croze@belledonne-communications.com>
2018-07-09 16:11:41 +0200

    Revert "Update prepare.py liblinphone-android-sdk in debug"
    This reverts commit 7d40fe9e2c09cf91b87d34ffdd888d3c04a38753.

--- prepare.py
+++ prepare.py
@@ -373,7 +373,7 @@ generate-javadoc:
 debug-sdk: java-clean build copy-libs generate-javadoc generate-apk
 \t./gradlew -q sdkZip
 
-liblinphone-android-sdk: java-clean build copy-libs generate-javadoc debug
+liblinphone-android-sdk: java-clean build copy-libs generate-javadoc release
 \t./gradlew -q sdkZip
 
 linphone-android-sdk: java-clean build copy-libs 
