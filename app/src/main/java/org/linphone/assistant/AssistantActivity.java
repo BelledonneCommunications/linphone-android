b9860a3739da12943cd2e35b5b0d6304a03ccb33
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-04-18 17:48:26 +0200

    Started country picker

--- app/src/main/java/org/linphone/assistant/AssistantActivity.java
+++ app/src/main/java/org/linphone/assistant/AssistantActivity.java
@@ -19,9 +19,14 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.telephony.TelephonyManager;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import org.linphone.R;
+import org.linphone.core.tools.Log;
 import org.linphone.utils.ThemableActivity;
 
 public abstract class AssistantActivity extends ThemableActivity {
@@ -31,6 +36,8 @@ public abstract class AssistantActivity extends ThemableActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 
         mStatusBar = findViewById(R.id.status);
         if (getResources().getBoolean(R.bool.assistant_hide_status_bar)) {
@@ -50,5 +57,32 @@ public abstract class AssistantActivity extends ThemableActivity {
                         finish();
                     }
                 });
+    }
+
+    protected void showPhoneNumberDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(getString(R.string.phone_number_info_title))
+                .setMessage(
+                        getString(R.string.phone_number_link_info_content)
+                                + "\n"
+                                + getString(
+                                        R.string.phone_number_link_info_content_already_account))
+                .show();
+    }
+
+    protected void showCountryPickerDialog() {
+        DialogFragment countryPickerFragment = CountryPickerFragment.instance();
+        countryPickerFragment.show(getFragmentManager(), "Country picker");
+    }
+
+    protected int getCountryIsoCode() {
+        try {
+            TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+            String countryIso = tm.getNetworkCountryIso();
+            return org.linphone.core.Utils.getCccFromIso(countryIso.toUpperCase());
+        } catch (Exception e) {
+            Log.e("[Assistant] " + e);
+        }
+        return -1;
     }
 }
