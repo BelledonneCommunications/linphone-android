418de7962e8e080f6ced6a8e5091b04bf242df72
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-04-19 15:51:29 +0200

    Finished email account creation

--- app/src/main/java/org/linphone/assistant/EmailAccountValidationAssistantActivity.java
+++ app/src/main/java/org/linphone/assistant/EmailAccountValidationAssistantActivity.java
@@ -20,14 +20,82 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import org.linphone.R;
+import org.linphone.core.AccountCreator;
+import org.linphone.core.AccountCreatorListenerStub;
+import org.linphone.core.tools.Log;
 
 public class EmailAccountValidationAssistantActivity extends AssistantActivity {
+    private TextView mEmail, mFinishCreation;
+
+    private AccountCreatorListenerStub mListener;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.assistant_email_account_validation);
+
+        mEmail = findViewById(R.id.send_email);
+        mEmail.setText(mAccountCreator.getEmail());
+
+        mFinishCreation = findViewById(R.id.assistant_check);
+        mFinishCreation.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mFinishCreation.setEnabled(false);
+
+                        AccountCreator.Status status = mAccountCreator.isAccountActivated();
+                        if (status != AccountCreator.Status.RequestOk) {
+                            Log.e("[Email Account Validation] activateAccount returned " + status);
+                            mFinishCreation.setEnabled(true);
+                            showGenericErrorDialog(status);
+                        }
+                    }
+                });
+
+        mListener =
+                new AccountCreatorListenerStub() {
+                    @Override
+                    public void onIsAccountActivated(
+                            AccountCreator creator, AccountCreator.Status status, String resp) {
+                        Log.i(
+                                "[Email Account Validation] onIsAccountActivated status is "
+                                        + status);
+                        if (status.equals(AccountCreator.Status.AccountActivated)) {
+                            createProxyConfigAndLeaveAssistant();
+                        } else if (status.equals(AccountCreator.Status.AccountNotActivated)) {
+                            Toast.makeText(
+                                            EmailAccountValidationAssistantActivity.this,
+                                            getString(R.string.assistant_account_not_validated),
+                                            Toast.LENGTH_LONG)
+                                    .show();
+                            mFinishCreation.setEnabled(true);
+                        } else {
+                            showGenericErrorDialog(status);
+                            mFinishCreation.setEnabled(true);
+                        }
+                    }
+                };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAccountCreator.addListener(mListener);
+
+        // Prevent user to go back, it won't be able to come back here after...
+        mBack.setEnabled(false);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mAccountCreator.removeListener(mListener);
     }
 }
