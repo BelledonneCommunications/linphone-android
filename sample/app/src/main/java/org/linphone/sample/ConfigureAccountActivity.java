9b10f0d9c2299dae76cb2cecd8de282a235d6a7d
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-05-04 09:45:37 +0200

    Started sample app

---
+++ sample/app/src/main/java/org/linphone/sample/ConfigureAccountActivity.java
@@ -0,0 +1,33 @@
+package org.linphone.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class ConfigureAccountActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
