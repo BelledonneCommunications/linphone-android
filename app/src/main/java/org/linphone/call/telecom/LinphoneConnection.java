0e600eaf47be28fa5c07b13a611f43313181f579
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-05-17 14:22:09 +0200

    Split classes into multiple files

---
+++ app/src/main/java/org/linphone/call/telecom/LinphoneConnection.java
@@ -0,0 +1,139 @@
+package org.linphone.call.telecom;
+
+/*
+LinphoneConnection.java
+Copyright (C) 2019 Belledonne Communications, Grenoble, France
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_ABORT;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_ACTION;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_ANSWER;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_BROADCAST;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_CALL_ID;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_END;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_HOLD;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_TERMINATE;
+import static org.linphone.call.telecom.LinphoneConnectionService.CS_TO_EXT_UNHOLD;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.telecom.Connection;
+import android.telecom.DisconnectCause;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+@TargetApi(Build.VERSION_CODES.M)
+public class LinphoneConnection extends Connection {
+    private LinphoneConnectionService mConnectionService;
+    private boolean mIsActive = false;
+    private String mCallId;
+
+    LinphoneConnection(LinphoneConnectionService connectionService) {
+        mConnectionService = connectionService;
+
+        int capabilities = getConnectionCapabilities();
+        capabilities |= CAPABILITY_MUTE;
+        capabilities |= CAPABILITY_SUPPORT_HOLD;
+        capabilities |= CAPABILITY_HOLD;
+        setConnectionCapabilities(capabilities);
+    }
+
+    public String getCallId() {
+        return mCallId;
+    }
+
+    public void setCallId(String mCallId) {
+        this.mCallId = mCallId;
+    }
+
+    public void setLocalActive(boolean isActive) {
+        mIsActive = isActive;
+    }
+
+    public boolean isLocalActive() {
+        return mIsActive;
+    }
+
+    @Override
+    public void onAbort() {
+        mConnectionService.destroyCall(this);
+        destroy();
+
+        sendLocalBroadcast(CS_TO_EXT_ABORT);
+    }
+
+    @Override
+    public void onAnswer(int videoState) {
+        if (mConnectionService.getCallsNb() > 1) {
+            mConnectionService.holdInActiveCalls(this);
+        }
+        mConnectionService.setAsActive(this);
+        sendLocalBroadcast(CS_TO_EXT_ANSWER);
+    }
+
+    @Override
+    public void onPlayDtmfTone(char c) {
+        // TODO: play/send DTMF
+    }
+
+    @Override
+    public void onStopDtmfTone() {}
+
+    @Override
+    public void onDisconnect() {
+        sendLocalBroadcast(CS_TO_EXT_TERMINATE);
+    }
+
+    @Override
+    public void onHold() {
+        if (mConnectionService.getCallsNb() > 1) {
+            mConnectionService.performSwitchCall();
+        } else {
+            setOnHold();
+            sendLocalBroadcast(CS_TO_EXT_HOLD);
+        }
+    }
+
+    @Override
+    public void onReject() {
+        setDisconnected(new DisconnectCause(DisconnectCause.REJECTED));
+        mConnectionService.destroyCall(this);
+        destroy();
+        sendLocalBroadcast(CS_TO_EXT_END);
+        super.onReject();
+    }
+
+    @Override
+    public void onUnhold() {
+        setActive();
+        sendLocalBroadcast(CS_TO_EXT_UNHOLD);
+    }
+
+    @Override
+    public void onStateChanged(int state) {
+        mConnectionService.updateCallCapabilities();
+        mConnectionService.updateConferenceable();
+    }
+
+    public void sendLocalBroadcast(int action) {
+        Intent intent = new Intent(CS_TO_EXT_BROADCAST);
+        intent.putExtra(CS_TO_EXT_ACTION, action);
+        intent.putExtra(CS_TO_EXT_CALL_ID, getCallId());
+        LocalBroadcastManager.getInstance(mConnectionService.getApplicationContext())
+                .sendBroadcast(intent);
+    }
+}
---
+++ app/src/main/java/org/linphone/call/telecom/TelecomBroadcastReceiver.java
@@ -0,0 +1,102 @@
+package org.linphone.call.telecom;
+
+/*
+TelecomBroadcastReceiver.java
+Copyright (C) 2019 Belledonne Communications, Grenoble, France
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import org.linphone.LinphoneManager;
+import org.linphone.LinphoneService;
+import org.linphone.core.Call;
+import org.linphone.core.CallLog;
+import org.linphone.core.Core;
+import org.linphone.core.tools.Log;
+
+public class TelecomBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        int action = intent.getIntExtra(LinphoneConnectionService.CS_TO_EXT_ACTION, -1);
+        String callId = intent.getStringExtra(LinphoneConnectionService.CS_TO_EXT_CALL_ID);
+
+        Call call = findCallFromId(callId);
+        Log.i(
+                "[Telecom Broadcast Receiver] Received action "
+                        + getActionNameFromValue(action)
+                        + " for call id "
+                        + callId
+                        + (call == null ? " (not found)" : " (found)"));
+        if (call == null) {
+            LinphoneService.instance().getTelecomHelper().onCallTerminated(callId);
+        }
+
+        switch (action) {
+            case LinphoneConnectionService.CS_TO_EXT_ANSWER:
+                LinphoneManager.getCallManager().acceptCall(call);
+                break;
+            case LinphoneConnectionService.CS_TO_EXT_END:
+                call.terminate();
+                break;
+            case LinphoneConnectionService.CS_TO_EXT_TERMINATE:
+                call.terminate();
+                break;
+            case LinphoneConnectionService.CS_TO_EXT_ABORT:
+                call.terminate();
+                break;
+            case LinphoneConnectionService.CS_TO_EXT_HOLD:
+                call.pause();
+                break;
+            case LinphoneConnectionService.CS_TO_EXT_UNHOLD:
+                call.resume();
+                break;
+        }
+    }
+
+    private String getActionNameFromValue(int action) {
+        switch (action) {
+            case LinphoneConnectionService.CS_TO_EXT_ANSWER:
+                return "CS_TO_EXT_ANSWER";
+            case LinphoneConnectionService.CS_TO_EXT_END:
+                return "CS_TO_EXT_END";
+            case LinphoneConnectionService.CS_TO_EXT_TERMINATE:
+                return "CS_TO_EXT_TERMINATE";
+            case LinphoneConnectionService.CS_TO_EXT_ABORT:
+                return "CS_TO_EXT_ABORT";
+            case LinphoneConnectionService.CS_TO_EXT_HOLD:
+                return "CS_TO_EXT_HOLD";
+            case LinphoneConnectionService.CS_TO_EXT_UNHOLD:
+                return "CS_TO_EXT_UNHOLD";
+        }
+        return "Unknown: " + action;
+    }
+
+    private Call findCallFromId(String callId) {
+        Core core = LinphoneManager.getCore();
+        if (callId == null) return core.getCurrentCall();
+
+        Call[] calls = core.getCalls();
+        for (Call call : calls) {
+            CallLog log = call.getCallLog();
+            if (log != null && callId.equals(log.getCallId())) {
+                return call;
+            }
+        }
+        return null;
+    }
+}
