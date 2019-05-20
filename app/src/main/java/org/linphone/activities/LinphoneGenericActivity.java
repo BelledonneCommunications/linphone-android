bcc12bb086d33675ae7335254b9bd1b436f93215
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-05-20 14:57:42 +0200

    Use library preview scaling + improved device rotation changes detection

--- app/src/main/java/org/linphone/activities/LinphoneGenericActivity.java
+++ app/src/main/java/org/linphone/activities/LinphoneGenericActivity.java
@@ -20,7 +20,11 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 import android.os.Bundle;
+import android.view.Surface;
+import org.linphone.LinphoneManager;
 import org.linphone.LinphoneService;
+import org.linphone.core.Core;
+import org.linphone.core.tools.Log;
 
 public abstract class LinphoneGenericActivity extends ThemeableActivity {
     protected boolean mAbortCreation;
@@ -35,6 +39,43 @@ public abstract class LinphoneGenericActivity extends ThemeableActivity {
             startActivity(getIntent().setClass(this, LinphoneLauncherActivity.class));
             mAbortCreation = true;
             finish();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (LinphoneService.isReady()) {
+            int degrees = 270;
+            int orientation = getWindowManager().getDefaultDisplay().getRotation();
+            switch (orientation) {
+                case Surface.ROTATION_0:
+                    degrees = 0;
+                    break;
+                case Surface.ROTATION_90:
+                    degrees = 270;
+                    break;
+                case Surface.ROTATION_180:
+                    degrees = 180;
+                    break;
+                case Surface.ROTATION_270:
+                    degrees = 90;
+                    break;
+            }
+
+            Log.i(
+                    "[Generic Activity] Device orientation is "
+                            + degrees
+                            + " (raw value is "
+                            + orientation
+                            + ")");
+
+            int rotation = (360 - degrees) % 360;
+            Core core = LinphoneManager.getCore();
+            if (core != null) {
+                core.setDeviceRotation(rotation);
+            }
         }
     }
 }
