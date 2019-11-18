5206e37a5a174f67ea09cbef9e78ae26968ebbf4
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-11-18 12:19:32 +0100

    Account creator changes

--- app/src/main/java/org/linphone/assistant/AccountConnectionAssistantActivity.java
+++ app/src/main/java/org/linphone/assistant/AccountConnectionAssistantActivity.java
@@ -72,17 +72,22 @@ public class AccountConnectionAssistantActivity extends AssistantActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        AccountCreator accountCreator = getAccountCreator();
+                        accountCreator.reset();
                         mConnect.setEnabled(false);
 
                         if (mUsernameConnectionSwitch.isChecked()) {
-                            mAccountCreator.setUsername(mUsername.getText().toString());
-                            mAccountCreator.setPassword(mPassword.getText().toString());
+                            accountCreator.setUsername(mUsername.getText().toString());
+                            accountCreator.setPassword(mPassword.getText().toString());
 
                             createProxyConfigAndLeaveAssistant();
                         } else {
-                            mAccountCreator.setUsername(mPhoneNumber.getText().toString());
+                            accountCreator.setPhoneNumber(
+                                    mPhoneNumber.getText().toString(),
+                                    mPrefix.getText().toString());
+                            accountCreator.setUsername(accountCreator.getPhoneNumber());
 
-                            AccountCreator.Status status = mAccountCreator.recoverAccount();
+                            AccountCreator.Status status = accountCreator.recoverAccount();
                             if (status != AccountCreator.Status.RequestOk) {
                                 Log.e(
                                         "[Account Connection Assistant] recoverAccount returned "
@@ -221,18 +226,18 @@ public class AccountConnectionAssistantActivity extends AssistantActivity {
                         }
                     }
                 };
-
-        Core core = LinphoneManager.getCore();
-        if (core != null) {
-            reloadLinphoneAccountCreatorConfig();
-        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        mAccountCreator.addListener(mListener);
+        Core core = LinphoneManager.getCore();
+        if (core != null) {
+            reloadLinphoneAccountCreatorConfig();
+        }
+
+        getAccountCreator().addListener(mListener);
 
         DialPlan dp = getDialPlanForCurrentCountry();
         displayDialPlan(dp);
@@ -246,7 +251,7 @@ public class AccountConnectionAssistantActivity extends AssistantActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        mAccountCreator.removeListener(mListener);
+        getAccountCreator().removeListener(mListener);
     }
 
     @Override
