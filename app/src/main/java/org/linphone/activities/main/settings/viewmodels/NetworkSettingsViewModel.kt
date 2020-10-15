d7f31b79cb480681f95531e81b1c69667f8e4b66
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2020-10-15 14:30:20 +0200

    Added setting for legacy push format + moved some configuration for CoreContext to CorePreferences

--- app/src/main/java/org/linphone/activities/main/settings/viewmodels/NetworkSettingsViewModel.kt
+++ app/src/main/java/org/linphone/activities/main/settings/viewmodels/NetworkSettingsViewModel.kt
@@ -46,6 +46,13 @@ class NetworkSettingsViewModel : GenericSettingsViewModel() {
     val pushNotifications = MutableLiveData<Boolean>()
     val pushNotificationsAvailable = MutableLiveData<Boolean>()
 
+    val legacyPushNotificationFormatListener = object : SettingListenerStub() {
+        override fun onBoolValueChanged(newValue: Boolean) {
+            prefs.useLegacyPushNotificationFormat = newValue
+        }
+    }
+    val legacyPushNotificationFormat = MutableLiveData<Boolean>()
+
     val randomPortsListener = object : SettingListenerStub() {
         override fun onBoolValueChanged(newValue: Boolean) {
             val port = if (newValue) -1 else 5060
@@ -71,6 +78,7 @@ class NetworkSettingsViewModel : GenericSettingsViewModel() {
         allowIpv6.value = core.ipv6Enabled()
         pushNotifications.value = core.isPushNotificationEnabled
         pushNotificationsAvailable.value = core.isPushNotificationAvailable
+        legacyPushNotificationFormat.value = prefs.useLegacyPushNotificationFormat
         randomPorts.value = getSipPort() == -1
         sipPort.value = getSipPort()
     }
