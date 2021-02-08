fe16aa91c0589e6777b42e189df9cf6bd5d661c4
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-02-08 17:10:07 +0100

    Added read & agreed to general terms and privacy policy mandatory checkbox in assistant

--- app/src/main/java/org/linphone/activities/assistant/fragments/WelcomeFragment.kt
+++ app/src/main/java/org/linphone/activities/assistant/fragments/WelcomeFragment.kt
@@ -19,8 +19,17 @@
  */
 package org.linphone.activities.assistant.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import java.util.regex.Pattern
+import org.linphone.LinphoneApplication.Companion.corePreferences
 import org.linphone.R
 import org.linphone.activities.GenericFragment
 import org.linphone.activities.assistant.viewmodels.WelcomeViewModel
@@ -63,5 +72,54 @@ class WelcomeFragment : GenericFragment<AssistantWelcomeFragmentBinding>() {
         binding.setRemoteProvisioningClickListener {
             navigateToRemoteProvisioning()
         }
+
+        viewModel.termsAndPrivacyAccepted.observe(viewLifecycleOwner, {
+            if (it) corePreferences.readAndAgreeTermsAndPrivacy = true
+        })
+
+        setUpTermsAndPrivacyLinks()
+    }
+
+    private fun setUpTermsAndPrivacyLinks() {
+        val terms = getString(R.string.assistant_general_terms)
+        val privacy = getString(R.string.assistant_privacy_policy)
+
+        val label = getString(
+            R.string.assistant_read_and_agree_terms,
+            terms,
+            privacy
+        )
+        val spannable = SpannableString(label)
+
+        val termsMatcher = Pattern.compile(terms).matcher(label)
+        if (termsMatcher.find()) {
+            val clickableSpan: ClickableSpan = object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    val browserIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(getString(R.string.assistant_general_terms_link))
+                    )
+                    startActivity(browserIntent)
+                }
+            }
+            spannable.setSpan(clickableSpan, termsMatcher.start(0), termsMatcher.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        val policyMatcher = Pattern.compile(privacy).matcher(label)
+        if (policyMatcher.find()) {
+            val clickableSpan: ClickableSpan = object : ClickableSpan() {
+                override fun onClick(widget: View) {
+                    val browserIntent = Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse(getString(R.string.assistant_privacy_policy_link))
+                    )
+                    startActivity(browserIntent)
+                }
+            }
+            spannable.setSpan(clickableSpan, policyMatcher.start(0), policyMatcher.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+
+        binding.termsAndPrivacy.text = spannable
+        binding.termsAndPrivacy.movementMethod = LinkMovementMethod.getInstance()
     }
 }
