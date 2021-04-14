6b1c61184557e9db117ad788113a00229fb22490
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-04-14 11:39:39 +0200

    Added debug popup entry to show config file

---
+++ app/src/main/java/org/linphone/activities/main/dialer/viewmodels/ConfigFileViewModel.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2010-2021 Belledonne Communications SARL.
+ *
+ * This file is part of linphone-android
+ * (see https://www.linphone.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.linphone.activities.main.dialer.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import org.linphone.LinphoneApplication.Companion.coreContext
+
+class ConfigFileViewModel : ViewModel() {
+    val text = MutableLiveData<String>()
+
+    init {
+        text.value = coreContext.core.config.dump()
+    }
+}
