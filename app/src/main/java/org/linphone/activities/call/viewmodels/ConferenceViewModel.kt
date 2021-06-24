7318a4fec248e8abfa6c3096e5617d48c0d40134
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-06-24 14:14:09 +0200

    Changes for video conferencing

--- app/src/main/java/org/linphone/activities/call/viewmodels/ConferenceViewModel.kt
+++ app/src/main/java/org/linphone/activities/call/viewmodels/ConferenceViewModel.kt
@@ -23,6 +23,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import org.linphone.LinphoneApplication.Companion.coreContext
 import org.linphone.activities.call.data.ConferenceParticipantData
+import org.linphone.activities.call.data.ConferenceParticipantDeviceData
 import org.linphone.core.*
 import org.linphone.core.tools.Log
 
@@ -34,8 +35,11 @@ class ConferenceViewModel : ViewModel() {
     val conferenceAddress = MutableLiveData<Address>()
 
     val conferenceParticipants = MutableLiveData<List<ConferenceParticipantData>>()
+    val conferenceParticipantDevices = MutableLiveData<List<ConferenceParticipantDeviceData>>()
 
     val isInConference = MutableLiveData<Boolean>()
+
+    val isVideoConference = MutableLiveData<Boolean>()
 
     private val conferenceListener = object : ConferenceListenerStub() {
         override fun onParticipantAdded(conference: Conference, participant: Participant) {
@@ -75,6 +79,7 @@ class ConferenceViewModel : ViewModel() {
         ) {
             Log.i("[Conference VM] Conference state changed: $state")
             isConferencePaused.value = !conference.isIn
+            isVideoConference.value = coreContext.core.conference?.currentParams?.isVideoEnabled
 
             if (state == Conference.State.Instantiated) {
                 conference.addListener(conferenceListener)
@@ -84,8 +89,10 @@ class ConferenceViewModel : ViewModel() {
                 conferenceAddress.value = conference.conferenceAddress
             } else if (state == Conference.State.Terminated || state == Conference.State.TerminationFailed) {
                 isInConference.value = false
+                isVideoConference.value = false
                 conference.removeListener(conferenceListener)
                 conferenceParticipants.value = arrayListOf()
+                conferenceParticipantDevices.value = arrayListOf()
             }
         }
     }
@@ -96,7 +103,9 @@ class ConferenceViewModel : ViewModel() {
         isConferencePaused.value = coreContext.core.conference?.isIn != true
         isMeConferenceFocus.value = false
         conferenceParticipants.value = arrayListOf()
+        conferenceParticipantDevices.value = arrayListOf()
         isInConference.value = false
+        isVideoConference.value = coreContext.core.conference?.currentParams?.isVideoEnabled
 
         val conference = coreContext.core.conference
         if (conference != null) {
@@ -108,6 +117,9 @@ class ConferenceViewModel : ViewModel() {
 
     override fun onCleared() {
         coreContext.core.removeListener(listener)
+
+        conferenceParticipants.value.orEmpty().forEach(ConferenceParticipantData::destroy)
+        conferenceParticipantDevices.value.orEmpty().forEach(ConferenceParticipantDeviceData::destroy)
 
         super.onCleared()
     }
@@ -145,13 +157,34 @@ class ConferenceViewModel : ViewModel() {
     }
 
     private fun updateParticipantsList(conference: Conference) {
+        conferenceParticipants.value.orEmpty().forEach(ConferenceParticipantData::destroy)
+        conferenceParticipantDevices.value.orEmpty().forEach(ConferenceParticipantDeviceData::destroy)
+
         val participants = arrayListOf<ConferenceParticipantData>()
-        for (participant in conference.participantList) {
-            Log.i("[Conference VM] Participant found: ${participant.address.asStringUriOnly()}")
-            val viewModel = ConferenceParticipantData(conference, participant)
-            participants.add(viewModel)
+        val devices = arrayListOf<ConferenceParticipantDeviceData>()
+
+        val participantsList = conference.participantList
+        Log.i("[Conference VM] Conference has ${participantsList.size} participants")
+        for (participant in participantsList) {
+            val participantDevices = participant.devices
+            Log.i("[Conference VM] Participant found: ${participant.address.asStringUriOnly()} with ${participantDevices.size} device(s)")
+
+            val participantData = ConferenceParticipantData(conference, participant)
+            participants.add(participantData)
+
+            if (!conference.isMe(participant.address)) {
+                for (device in participantDevices) {
+                    Log.i("[Conference VM] Participant device found: ${device.name} (${device.address.asStringUriOnly()})")
+                    val deviceData = ConferenceParticipantDeviceData(device)
+                    devices.add(deviceData)
+                }
+            } else {
+                Log.i("[Conference VM] Not adding our own devices...")
+            }
         }
+
         conferenceParticipants.value = participants
+        conferenceParticipantDevices.value = devices
         isInConference.value = participants.isNotEmpty()
     }
 }
