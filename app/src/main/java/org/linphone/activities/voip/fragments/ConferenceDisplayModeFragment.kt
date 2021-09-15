44f54aef256b99133abd5a212f0e00b54a83deaf
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-09-15 11:44:23 +0200

    New call/conference UI

---
+++ app/src/main/java/org/linphone/activities/voip/fragments/ConferenceDisplayModeFragment.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2010-2021 Belledonne Communications SARL.
+ *
+ * This file is part of linphone-android
+ * (see https://www.linphone.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.linphone.activities.voip.fragments
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.navGraphViewModels
+import org.linphone.R
+import org.linphone.activities.GenericFragment
+import org.linphone.activities.voip.viewmodels.ConferenceViewModel
+import org.linphone.core.ConferenceLayout
+import org.linphone.core.tools.Log
+import org.linphone.databinding.VoipConferenceDisplayModeFragmentBinding
+
+class ConferenceDisplayModeFragment : GenericFragment<VoipConferenceDisplayModeFragmentBinding>() {
+    private val conferenceViewModel: ConferenceViewModel by navGraphViewModels(R.id.call_nav_graph)
+
+    override fun getLayoutId(): Int = R.layout.voip_conference_display_mode_fragment
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.conferenceViewModel = conferenceViewModel
+
+        binding.setCancelClickListener {
+            goBack()
+        }
+
+        conferenceViewModel.conferenceMosaicDisplayMode.observe(
+            viewLifecycleOwner,
+            {
+                if (it) {
+                    Log.i("[Conference] Trying to change conference layout to Grid")
+                    val conference = conferenceViewModel.conference.value
+                    if (conference != null) {
+                        conference.layout = ConferenceLayout.Grid
+                    } else {
+                        Log.e("[Conference] Conference is null in ConferenceViewModel")
+                    }
+                }
+            }
+        )
+
+        conferenceViewModel.conferenceActiveSpeakerDisplayMode.observe(
+            viewLifecycleOwner,
+            {
+                if (it) {
+                    Log.i("[Conference] Trying to change conference layout to ActiveSpeaker")
+                    val conference = conferenceViewModel.conference.value
+                    if (conference != null) {
+                        conference.layout = ConferenceLayout.ActiveSpeaker
+                    } else {
+                        Log.e("[Conference] Conference is null in ConferenceViewModel")
+                    }
+                }
+            }
+        )
+    }
+}
