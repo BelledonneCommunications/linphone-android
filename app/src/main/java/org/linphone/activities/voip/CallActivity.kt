7648212f6fe13a066e2e97a662096845ee2dd564
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2021-09-15 11:44:23 +0200

    New call/conference UI

---
+++ app/src/main/java/org/linphone/activities/voip/CallActivity.kt
@@ -0,0 +1,241 @@
+/*
+ * Copyright (c) 2010-2021 Belledonne Communications SARL.
+ *
+ * This file is part of linphone-android
+ * (see https://www.linphone.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.linphone.activities.voip
+
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.window.layout.FoldingFeature
+import kotlinx.coroutines.*
+import org.linphone.LinphoneApplication.Companion.coreContext
+import org.linphone.LinphoneApplication.Companion.corePreferences
+import org.linphone.R
+import org.linphone.activities.call.ProximitySensorActivity
+import org.linphone.activities.call.viewmodels.SharedCallViewModel
+import org.linphone.activities.main.MainActivity
+import org.linphone.activities.voip.viewmodels.CallsViewModel
+import org.linphone.activities.voip.viewmodels.ControlsViewModel
+import org.linphone.compatibility.Compatibility
+import org.linphone.core.tools.Log
+import org.linphone.databinding.VoipActivityBinding
+import org.linphone.mediastream.Version
+import org.linphone.utils.PermissionHelper
+
+class CallActivity : ProximitySensorActivity() {
+    private lateinit var binding: VoipActivityBinding
+    private lateinit var sharedViewModel: SharedCallViewModel
+    private lateinit var controlsViewModel: ControlsViewModel
+    private lateinit var callsViewModel: CallsViewModel
+
+    private var foldingFeature: FoldingFeature? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Compatibility.setShowWhenLocked(this, true)
+        Compatibility.setTurnScreenOn(this, true)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.voip_activity)
+        binding.lifecycleOwner = this
+
+        sharedViewModel = ViewModelProvider(this).get(SharedCallViewModel::class.java)
+
+        sharedViewModel.toggleDrawerEvent.observe(
+            this,
+            {
+                it.consume {
+                    if (binding.statsMenu.isDrawerOpen(Gravity.LEFT)) {
+                        binding.statsMenu.closeDrawer(binding.sideMenuContent, true)
+                    } else {
+                        binding.statsMenu.openDrawer(binding.sideMenuContent, true)
+                    }
+                }
+            }
+        )
+
+        controlsViewModel = ViewModelProvider(this).get(ControlsViewModel::class.java)
+        binding.controlsViewModel = controlsViewModel
+
+        callsViewModel = ViewModelProvider(this).get(CallsViewModel::class.java)
+
+        callsViewModel.noMoreCallEvent.observe(
+            this,
+            {
+                it.consume {
+                    finish()
+                }
+            }
+        )
+
+        controlsViewModel.askPermissionEvent.observe(
+            this,
+            {
+                it.consume { permission ->
+                    Log.i("[Call] Asking for $permission permission")
+                    requestPermissions(arrayOf(permission), 0)
+                }
+            }
+        )
+
+        if (Version.sdkAboveOrEqual(Version.API23_MARSHMALLOW_60)) {
+            checkPermissions()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (coreContext.core.callsNb == 0) {
+            Log.w("[Call] Resuming but no call found...")
+            if (isTaskRoot) {
+                // When resuming app from recent tasks make sure MainActivity will be launched if there is no call
+                val intent = Intent()
+                intent.setClass(this, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            } else {
+                finish()
+            }
+        } else {
+            coreContext.removeCallOverlay()
+        }
+
+        if (corePreferences.fullScreenCallUI) {
+            hideSystemUI()
+            window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+                if (visibility and View.SYSTEM_UI_FLAG_FULLSCREEN == 0) {
+                    lifecycleScope.launch {
+                        delay(2000)
+                        withContext(Dispatchers.Main) {
+                            hideSystemUI()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        val core = coreContext.core
+        if (core.callsNb > 0) {
+            coreContext.createCallOverlay()
+        }
+
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        coreContext.core.nativeVideoWindowId = null
+        coreContext.core.nativePreviewWindowId = null
+
+        super.onDestroy()
+    }
+
+    override fun getTheme(): Resources.Theme {
+        val theme = super.getTheme()
+        if (corePreferences.fullScreenCallUI) {
+            theme.applyStyle(R.style.FullScreenTheme, true)
+        }
+        return theme
+    }
+
+    @TargetApi(Version.API23_MARSHMALLOW_60)
+    private fun checkPermissions() {
+        val permissionsRequiredList = arrayListOf<String>()
+        if (!PermissionHelper.get().hasRecordAudioPermission()) {
+            Log.i("[Call] Asking for RECORD_AUDIO permission")
+            permissionsRequiredList.add(Manifest.permission.RECORD_AUDIO)
+        }
+        if (callsViewModel.currentCallData.value?.call?.currentParams?.videoEnabled() == true &&
+            !PermissionHelper.get().hasCameraPermission()
+        ) {
+            Log.i("[Call] Asking for CAMERA permission")
+            permissionsRequiredList.add(Manifest.permission.CAMERA)
+        }
+        if (permissionsRequiredList.isNotEmpty()) {
+            val permissionsRequired = arrayOfNulls<String>(permissionsRequiredList.size)
+            permissionsRequiredList.toArray(permissionsRequired)
+            requestPermissions(permissionsRequired, 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 0) {
+            for (i in permissions.indices) {
+                when (permissions[i]) {
+                    Manifest.permission.RECORD_AUDIO -> if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        Log.i("[Call] RECORD_AUDIO permission has been granted")
+                        controlsViewModel.updateMicState()
+                    }
+                    Manifest.permission.CAMERA -> if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                        Log.i("[Call] CAMERA permission has been granted")
+                        coreContext.core.reloadVideoDevices()
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onLayoutChanges(foldingFeature: FoldingFeature?) {
+        this.foldingFeature = foldingFeature
+        updateConstraintSetDependingOnFoldingState()
+    }
+
+    private fun hideSystemUI() {
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+            View.SYSTEM_UI_FLAG_IMMERSIVE or
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    }
+
+    private fun updateConstraintSetDependingOnFoldingState() {
+        /*val feature = foldingFeature ?: return
+        val constraintLayout = binding.constraintLayout
+        val set = ConstraintSet()
+        set.clone(constraintLayout)
+
+        if (feature.state == FoldingFeature.State.HALF_OPENED && viewModel.videoEnabled.value == true) {
+            set.setGuidelinePercent(R.id.hinge_top, 0.5f)
+            set.setGuidelinePercent(R.id.hinge_bottom, 0.5f)
+            viewModel.disable(true)
+        } else {
+            set.setGuidelinePercent(R.id.hinge_top, 0f)
+            set.setGuidelinePercent(R.id.hinge_bottom, 1f)
+            viewModel.disable(false)
+        }
+
+        set.applyTo(constraintLayout)*/
+    }
+}
