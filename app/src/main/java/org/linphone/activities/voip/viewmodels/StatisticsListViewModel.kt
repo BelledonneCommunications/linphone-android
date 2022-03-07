4c616cc395bd79ecc65822cf3fbd791f2a3c7e64
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2022-03-07 15:06:14 +0100

    Improved call statistics: only compute them when dialog is visible + fixed leak

--- app/src/main/java/org/linphone/activities/voip/viewmodels/StatisticsListViewModel.kt
+++ app/src/main/java/org/linphone/activities/voip/viewmodels/StatisticsListViewModel.kt
@@ -30,6 +30,8 @@ import org.linphone.core.CoreListenerStub
 class StatisticsListViewModel : ViewModel() {
     val callStatsList = MutableLiveData<ArrayList<CallStatisticsData>>()
 
+    private var enabled = false
+
     private val listener = object : CoreListenerStub() {
         override fun onCallStateChanged(
             core: Core,
@@ -49,6 +51,20 @@ class StatisticsListViewModel : ViewModel() {
         computeCallsList()
     }
 
+    fun enable() {
+        enabled = true
+        for (stat in callStatsList.value.orEmpty()) {
+            stat.enable()
+        }
+    }
+
+    fun disable() {
+        enabled = false
+        for (stat in callStatsList.value.orEmpty()) {
+            stat.disable()
+        }
+    }
+
     override fun onCleared() {
         callStatsList.value.orEmpty().forEach(CallStatisticsData::destroy)
         coreContext.core.removeListener(listener)
@@ -57,12 +73,19 @@ class StatisticsListViewModel : ViewModel() {
     }
 
     private fun computeCallsList() {
+        callStatsList.value.orEmpty().forEach(CallStatisticsData::destroy)
+
         val list = arrayListOf<CallStatisticsData>()
         for (call in coreContext.core.calls) {
             if (call.state != Call.State.End && call.state != Call.State.Released && call.state != Call.State.Error) {
-                list.add(CallStatisticsData(call))
+                val data = CallStatisticsData(call)
+                list.add(data)
+                if (enabled) {
+                    data.enable()
+                }
             }
         }
+
         callStatsList.value = list
     }
 }
