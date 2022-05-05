209f577890448790a366b3a5919e42d08eb63b25
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2022-05-05 09:25:59 +0200

    Fixed crash when device boots & start at boot setting is disabled

--- app/src/main/java/org/linphone/core/BootReceiver.kt
+++ app/src/main/java/org/linphone/core/BootReceiver.kt
@@ -36,16 +36,12 @@ class BootReceiver : BroadcastReceiver() {
             Log.i("[Boot Receiver] Device is starting, autoStart is $autoStart")
             if (autoStart) {
                 startService(context)
-            } else {
-                stopService()
             }
         } else if (intent.action.equals(Intent.ACTION_MY_PACKAGE_REPLACED, ignoreCase = true)) {
             val autoStart = corePreferences.autoStart
             Log.i("[Boot Receiver] App has been updated, autoStart is $autoStart")
             if (autoStart) {
                 startService(context)
-            } else {
-                stopService()
             }
         }
     }
@@ -61,11 +57,5 @@ class BootReceiver : BroadcastReceiver() {
         val serviceIntent = Intent(Intent.ACTION_MAIN).setClass(context, CoreService::class.java)
         serviceIntent.putExtra("StartForeground", true)
         Compatibility.startForegroundService(context, serviceIntent)
-    }
-
-    private fun stopService() {
-        Log.i("[Boot Receiver] Auto start setting is disabled, stopping foreground service")
-        coreContext.notificationsManager.stopForegroundNotification()
-        coreContext.stop()
     }
 }
