0fea3b802f770c55bf74d0fa7fa16981446bea1e
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2022-06-13 14:03:06 +0200

    Should prevent ANRs

--- app/src/main/java/org/linphone/activities/main/chat/data/EventLogData.kt
+++ app/src/main/java/org/linphone/activities/main/chat/data/EventLogData.kt
@@ -23,7 +23,11 @@ import org.linphone.contact.GenericContactData
 import org.linphone.core.EventLog
 
 class EventLogData(val eventLog: EventLog) {
-    val data: GenericContactData = if (eventLog.type == EventLog.Type.ConferenceChatMessage) {
+    val type: EventLog.Type = eventLog.type
+
+    val notifyId = eventLog.notifyId
+
+    val data: GenericContactData = if (type == EventLog.Type.ConferenceChatMessage) {
         ChatMessageData(eventLog.chatMessage!!)
     } else {
         EventData(eventLog)
