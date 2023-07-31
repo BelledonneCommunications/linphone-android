f7f5a3cf5033f657260eca89aba46e095dbb033e
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2023-07-31 13:50:43 +0200

    Started contacts list

---
+++ app/src/main/java/org/linphone/ui/contacts/ContactsFragment.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2010-2023 Belledonne Communications SARL.
+ *
+ * This file is part of linphone-android
+ * (see https://www.linphone.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.linphone.ui.contacts
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import androidx.navigation.navGraphViewModels
+import org.linphone.R
+import org.linphone.databinding.ContactsFragmentBinding
+import org.linphone.ui.contacts.viewmodel.ContactsListViewModel
+
+class ContactsFragment : Fragment() {
+    private lateinit var binding: ContactsFragmentBinding
+    private val listViewModel: ContactsListViewModel by navGraphViewModels(
+        R.id.contactsFragment
+    )
+
+    override fun onCreateAnimation(transit: Int, enter: Boolean, nextAnim: Int): Animation? {
+        // Holds fragment in place while new fragment slides over it
+        return AnimationUtils.loadAnimation(activity, R.anim.hold)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = ContactsFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = listViewModel
+
+        // postponeEnterTransition()
+    }
+}
