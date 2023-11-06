46ae3267812d19598d718d22b87c6180a6c0f2bf
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2023-11-06 12:10:35 +0100

    Improved chat image viewer

--- app/src/main/java/org/linphone/ui/main/viewer/viewmodel/FileViewModel.kt
+++ app/src/main/java/org/linphone/ui/main/viewer/viewmodel/FileViewModel.kt
@@ -7,7 +7,17 @@ import androidx.lifecycle.ViewModel
 class FileViewModel @UiThread constructor() : ViewModel() {
     val path = MutableLiveData<String>()
 
+    val fullScreenMode = MutableLiveData<Boolean>()
+
+    init {
+        fullScreenMode.value = true
+    }
+
     fun loadFile(file: String) {
         path.postValue(file)
+    }
+
+    fun toggleFullScreen() {
+        fullScreenMode.value = fullScreenMode.value != true
     }
 }
