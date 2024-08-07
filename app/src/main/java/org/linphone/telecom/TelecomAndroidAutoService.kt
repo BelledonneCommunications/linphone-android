a676c514012391a3ff8083aef75f38003c39cb90
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2024-08-07 10:00:10 +0200

    Finally managed to make call notifications visible on Android Auto!

---
+++ app/src/main/java/org/linphone/telecom/TelecomAndroidAutoService.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2010-2024 Belledonne Communications SARL.
+ *
+ * This file is part of linphone-android
+ * (see https://www.linphone.org).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.linphone.telecom
+
+import android.content.pm.ApplicationInfo
+import androidx.car.app.CarAppService
+import androidx.car.app.validation.HostValidator
+import org.linphone.R
+
+class TelecomAndroidAutoService : CarAppService() {
+    override fun createHostValidator(): HostValidator {
+        return if ((applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+            HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+        } else {
+            HostValidator.Builder(applicationContext)
+                .addAllowedHosts(R.array.hosts_allowlist_sample_copy)
+                .build()
+        }
+    }
+}
