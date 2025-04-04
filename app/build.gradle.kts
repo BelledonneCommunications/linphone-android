910527ef1ba8f0b21f46339950f34009167f5fa3
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2025-04-04 12:59:34 +0200

    Updated CHANGELOG & version code from release/6.0 branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ if (firebaseCloudMessagingAvailable) {
 }
 
 var gitBranch = ByteArrayOutputStream()
-var gitVersion = "6.0.2"
+var gitVersion = "6.0.3"
 
 task("getGitVersion") {
     val gitVersionStream = ByteArrayOutputStream()
@@ -100,8 +100,8 @@ android {
         applicationId = packageName
         minSdk = 28
         targetSdk = 36
-        versionCode = 600002 // 6.00.002
-        versionName = "6.0.2"
+        versionCode = 600003 // 6.00.003
+        versionName = "6.0.3"
 
         manifestPlaceholders["appAuthRedirectScheme"] = packageName
 
