b5947b89c44b6392857a105922a118a0ab274052
Chris Headington <chris.headington@dimensionstechnologies.com>
2025-07-25 15:07:31 +0100

    Improved call history styling.

--- app/src/main/java/org/linphone/utils/textStyleBindingAdapter.kt
+++ app/src/main/java/org/linphone/utils/textStyleBindingAdapter.kt
@@ -5,7 +5,7 @@ import androidx.databinding.BindingAdapter
 
 @BindingAdapter("textStyle")
 fun setTextStyle(textView: TextView, styleResId: Int) {
-    if (styleResId != 0) { // Ensure a valid style resource ID is provided
+    if (styleResId > 0) { // Ensure a valid style resource ID is provided
         textView.setTextAppearance(styleResId) // Apply the style programmatically
     }
 }
