6f09853424eaacaa9f454fa5be6900e8501932ef
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2026-01-05 10:40:35 +0100

    Fixed attaching file to conversation from third party app using shortcut if matching conversation is already displayed

--- app/src/main/java/org/linphone/ui/main/chat/fragment/ConversationFragment.kt
+++ app/src/main/java/org/linphone/ui/main/chat/fragment/ConversationFragment.kt
@@ -509,6 +509,8 @@ open class ConversationFragment : SlidingPaneChildFragment() {
                         )
                     }
                 } else {
+                    sharedViewModel.displayedChatRoom = viewModel.chatRoom
+
                     sendMessageViewModel.configureChatRoom(viewModel.chatRoom)
                     adapter.setIsConversationSecured(viewModel.isEndToEndEncrypted.value == true)
 
