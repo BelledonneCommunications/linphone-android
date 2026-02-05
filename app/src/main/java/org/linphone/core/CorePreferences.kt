bef6b4340a8e4ab1dfc6e5873948b9dcafc71ba0
Luke <dvduc@humaxdigital.com>
2026-02-05 16:11:21 +0700

    Set secure mode default to false

--- app/src/main/java/org/linphone/core/CorePreferences.kt
+++ app/src/main/java/org/linphone/core/CorePreferences.kt
@@ -309,7 +309,7 @@ class CorePreferences
     // Allows to make screenshots
     @get:AnyThread @set:WorkerThread
     var enableSecureMode: Boolean
-        get() = config.getBool("ui", "enable_secure_mode", true)
+        get() = config.getBool("ui", "enable_secure_mode", false)
         set(value) {
             config.setBool("ui", "enable_secure_mode", value)
         }
