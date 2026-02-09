799948ae87d999a221e55052d7e72a596de2997e
Luke <dvduc@humaxdigital.com>
2026-02-09 17:05:52 +0700

    Release 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,7 +108,7 @@ android {
         minSdk = 28
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.2"
 
         manifestPlaceholders["appAuthRedirectScheme"] = packageName
 
