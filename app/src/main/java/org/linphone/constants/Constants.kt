9b40a0b0d44cdcd0a0d860b7b3f9b07b2f518b5b
Luke <dvduc@humaxdigital.com>
2026-03-13 15:34:51 +0700

    [Setting] Show setting with automatic start recording calls only

--- app/src/main/java/org/linphone/constants/Constants.kt
+++ app/src/main/java/org/linphone/constants/Constants.kt
@@ -3,5 +3,5 @@ package org.linphone.constants
 const val DEFAULT_HIDE_CONVERSATION = true
 const val DISABLE_VIDEO_CALL = true
 const val DISABLE_RECORDING = false
-const val DISABLE_SETTINGS = true
+const val DISABLE_SETTINGS = false
 const val SHOW_CONTACTS_FILTER = false
