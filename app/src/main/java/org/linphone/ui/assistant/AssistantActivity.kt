b2fe49060b58f0dd61008ce5c9c388e36a7f270f
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2026-05-12 09:23:40 +0200

    Prevent being stuck in the Help page while in the Assistant if there's no account configured

--- app/src/main/java/org/linphone/ui/assistant/AssistantActivity.kt
+++ app/src/main/java/org/linphone/ui/assistant/AssistantActivity.kt
@@ -22,6 +22,7 @@ package org.linphone.ui.assistant
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.UiThread
@@ -51,6 +52,15 @@ class AssistantActivity : GenericActivity() {
 
     private lateinit var binding: AssistantActivityBinding
 
+    private val backPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            val navController = binding.assistantNavContainer.findNavController()
+            if (navController.currentDestination?.id != R.id.landingFragment) {
+                navController.popBackStack()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
@@ -75,8 +85,9 @@ class AssistantActivity : GenericActivity() {
             if (core.accountList.isEmpty()) {
                 Log.i("$TAG No account configured, disabling back gesture")
                 coreContext.postOnMainThread {
-                    // Disable back gesture / button
-                    onBackPressedDispatcher.addCallback { }
+                    // Only allow to navigate back within the assistant nav controller,
+                    // not to leave the AssistantActivity
+                    onBackPressedDispatcher.addCallback(backPressedCallback)
                 }
             }
         }
